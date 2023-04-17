c20a08b874ef2619eaa155d7e9f51adc428ca4ae
Fredray-21 <dabadie.frederic@gmail.com>
2023-04-17 15:32:52 +0200

    WIP(loader) : start loader mediapipe

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,13 +3,12 @@ const formesWrapper = document.querySelectorAll(".forme__wrapper");
 document.addEventListener("mousemove", e => {
   const x = e.clientX / window.innerWidth;
   const y = e.clientY / window.innerHeight;
-    
+
   formesWrapper.forEach(formeWrapper => {
     const speed = -50;
 
     const xPosition = (x - 0.5) * speed;
     const yPosition = (y - 0.5) * speed;
-    
 
     formeWrapper.style.transform = `translate(${xPosition}px, ${yPosition}px)`;
   });
