11b2de6861f400498549f6029da68a0692541191
Chocolatine <64270816+Chocolatiines@users.noreply.github.com>
2023-04-17 14:20:12 +0200

    update script couleurs de fond sens inverse

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,7 +5,7 @@ document.addEventListener("mousemove", e => {
   const y = e.clientY / window.innerHeight;
     
   formesWrapper.forEach(formeWrapper => {
-    const speed = 30;
+    const speed = -50;
 
     const xPosition = (x - 0.5) * speed;
     const yPosition = (y - 0.5) * speed;
