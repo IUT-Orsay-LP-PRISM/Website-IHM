5b88969810ab2144cb4a95cfac2562d6bae32c21
Fredray-21 <dabadie.frederic@gmail.com>
2023-06-07 16:04:34 +0200

    fix(export) : fix de l'image exporter rotation de l'image

--- assets/scripts/export.js
+++ assets/scripts/export.js
@@ -12,7 +12,25 @@ if (excanva){
     excanva.addEventListener('click', () => {
         let canva = document.getElementById("myCanva");
         let ctx = canva.getContext("2d");
-        let dataURL = canva.toDataURL("image/jpeg");
+        
+        // Créer un canvas temporaire
+        let tempCanvas = document.createElement("canvas");
+        let tempCtx = tempCanvas.getContext("2d");
+        tempCanvas.width = canva.width;
+        tempCanvas.height = canva.height;
+        
+        // Effectuer une translation horizontale pour déplacer l'origine
+        tempCtx.translate(tempCanvas.width, 0);
+        
+        // Effectuer une mise à l'échelle négative pour inverser l'image horizontalement
+        tempCtx.scale(-1, 1);
+        
+        // Dessiner le contenu du canvas actuel sur le canvas temporaire avec la transformation
+        tempCtx.drawImage(canva, 0, 0);
+        
+        // Exporter l'image du canvas temporaire
+        let dataURL = tempCanvas.toDataURL("image/jpeg");
+        
         let date = new Date();
         let chaine = date.toISOString().slice(0, 10) + "-" + date.getDay() + "-" + date.toISOString().slice(11, 23).replace("T", "-");
         localStorage.setItem("image_" + chaine, dataURL);
@@ -22,3 +40,4 @@ if (excanva){
         a.click();
     })
 }
+
